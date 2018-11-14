a99ecbb2503eb0b15237c45951359c74963e2326
Brook Frye <bfrye@council.nyc.gov>
2018-11-14 16:02:06 -0500

    getting sheet names and info via fun

--- Functions_HC.R
+++ Functions_HC.R
@@ -7,7 +7,7 @@ makeNamedList <- function(...) {
 read_excel_allsheets <- function(filename, tibble = FALSE) {
   sheets <- readxl::excel_sheets(filename)
   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
-  if(!tibble) x <- lapply(x, as.data.frame)
+  if(!tibble) x <- lapply(x, as.data.table)
   names(x) <- sheets
   x
 }
--- Hate_Crimes_explore.R
+++ Hate_Crimes_explore.R
@@ -1,38 +1,33 @@
 library(data.table)
 library(readxl)
-
-
+library(stringr)
+library(xlsx)
 source("Functions_HC.R")
+
 # Hate Crime analysis from NYPD data 
 # we want to look at the change of complaints over time & potentially broken down by gender/race/type 
 # read the data 
 
-# aggregate data 
-arrestfiles <- list.files(pattern = "arrests")
-arrests <- lapply(arrestfiles, read_excel, col_names = FALSE)
-
+### complaints 
 
 # let's look at total complaints and by motivation 
 complaintfiles <- list.files(pattern = "complaints")
 quarters <- complaintfiles[grep("-q", complaintfiles)]
 
-
-# let's look at motivation 
+# let's look at complaints by motivation 
 inds <- grep("by-motivation", quarters)
 motivations <- quarters[inds]
-
-
-
-
 motlist <- lapply(motivations, read_excel_allsheets)
-motlist2 <- makeNamedList(motlist)
+
+# motlist2 <- makeNamedList(motlist)
+for(i in 1:length(motivations)){
+wb <- loadWorkbook(motivations[i])
+sheets <- getSheets(wb) 
+}
+
+for(i in 1:6){
+  motlist[[i]] <- readLines()
   
+}
 
-
-
-# let's try to parse the years 
-
-
-
-# let's look at totals 
-totals <- quarters[!inds]
+### arrests 
