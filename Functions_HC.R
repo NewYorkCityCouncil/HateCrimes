ed5b54e14a678d86d9fa67671c5227375e69fcf9
Brook Frye <bfrye@council.nyc.gov>
2018-11-14 15:15:31 -0500

    first commit

---
+++ Functions_HC.R
@@ -0,0 +1,13 @@
+# hate crime functions 
+
+makeNamedList <- function(...) {
+  structure(list(...), names = as.list(substitute(list(...)))[-1L])
+}
+
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
--- Hate_Crimes_explore.R
+++ Hate_Crimes_explore.R
@@ -1,1 +1,38 @@
+library(data.table)
+library(readxl)
+
+
+source("Functions_HC.R")
 # Hate Crime analysis from NYPD data 
+# we want to look at the change of complaints over time & potentially broken down by gender/race/type 
+# read the data 
+
+# aggregate data 
+arrestfiles <- list.files(pattern = "arrests")
+arrests <- lapply(arrestfiles, read_excel, col_names = FALSE)
+
+
+# let's look at total complaints and by motivation 
+complaintfiles <- list.files(pattern = "complaints")
+quarters <- complaintfiles[grep("-q", complaintfiles)]
+
+
+# let's look at motivation 
+inds <- grep("by-motivation", quarters)
+motivations <- quarters[inds]
+
+
+
+
+motlist <- lapply(motivations, read_excel_allsheets)
+motlist2 <- makeNamedList(motlist)
+  
+
+
+
+# let's try to parse the years 
+
+
+
+# let's look at totals 
+totals <- quarters[!inds]
